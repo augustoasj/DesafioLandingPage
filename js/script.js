554beab4078b57b04e75eab03e643636725d5512
AugustoSilvaJr <augustus.junior@yahoo.com.br>
2021-07-18 11:51:55 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,29 @@
+let form = document.getElementById('form');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault;
+
+    let nome = document.getElementById('nome').value;
+    let email = document.getElementById('email').value;
+    let telefone = document.getElementById('telefone').value;
+    //console.log(nome, email, telefone)
+
+    /*Armazenando dados no LocalStorage*/
+    let dados = {
+        nome, email, telefone
+    }
+    let converterDados = JSON.stringify(dados)
+    localStorage.setItem('lead', converterDados)
+
+    //criando loading
+    let conteudo = document.getElementById('conteudo')
+
+    let cadastrando = `<h2> Cadastrando </h2>`
+    let concluido = `<h2> Concluido </h2>`
+    conteudo.innerHTML = cadastrando
+
+    setTimeout(() => {
+        conteudo.innerHTML = concluido
+    }, 1000)
+
+})
